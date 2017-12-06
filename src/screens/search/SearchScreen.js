e2ef581aa03f9d3ebe73d45a036cf822c80de659
ChenPei <315534868@qq.com>
2017-12-06 16:24:45 +0800

    fix search page

--- src/screens/search/SearchScreen.js
+++ src/screens/search/SearchScreen.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import { View, Image, TextInput } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons/';
+import { Icon, Button, Tabs, Tab } from 'native-base';
+import Colors from '../../../constants/Colors';
+import styles from './styles/SearchScreen';
 
 class SearchScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    header: ( 
+      <View style={{ flexDirection: 'row', borderBottomColor: Colors.$navigationHeaderTextColor, borderBottomWidth: 3 }}>
+        <Image source={require('../../../assets/imgs/logo.png')} style={styles.logo} />
+        <View style={{ width: '90%', flexDirection: 'row', marginLeft: '2%', alignItems: 'center', position: 'relative' }}>
+          <TextInput 
+            placeholder="请输入关键词 按Enter搜索" 
+            underlineColorAndroid='transparent'
+            style={{
+              borderRadius: 20, 
+              margin: '2%', 
+              marginLeft: '2%', 
+              padding: '1%', 
+              height: 30, 
+              borderWidth: 1, 
+              borderColor: '#ccc',
+              width: '85%',
+              backgroundColor: '#EEEEEE', 
+            }}
+          />
+          <Icon name="ios-search" style={{ position: 'absolute', right: '14%', fontSize: 20 }} /> 
+          <Button transparent onPress={() => navigation.goBack()} style={{ marginLeft: '3%', width: 26, height: 26, borderRadius: 13, backgroundColor: 'red' }}>
+            <FontAwesome name="remove" size={10} style={{ fontSize: 20, color: Colors.$navigationHeaderTextColor }} />
+          </Button> 
+        </View> 
+      </View>
+    ),
+  });
   render() {
     return (
-      <View>
-        <SearchBar
-          lightTheme
-          onChangeText={this.search}
-          placeholder='Type Here...'
-        />
+      <View style={styles.root}>
+        <Tabs initialPage={0} >
+          <Tab heading='搜行情' />
+          <Tab heading='搜新闻' />
+        </Tabs>
       </View>
+
     );
   }
 }
